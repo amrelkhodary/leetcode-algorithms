a74c603051c8052229bbbb96678f613092938611
amrelkhodary <amrelkhodarybusiness@gmail.com>
2024-08-21 09:57:34 +0300

    added lps code

---
+++ needle-in-haystack-kmp/code/solution.c
@@ -0,0 +1,29 @@
+/*
+  this is the solution file for the needle in a haystack problem
+  in leetcode using the KMP algorithm
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int KMP(char* needle, char* haystack);
+
+int main(){
+
+}
+
+int KMP(char* needle, char* haystack) {
+  if(needle == "") {
+    return 0; //the first matching char will be the first one
+  }
+
+  //populate the LPS
+  array
+  int needle_length = strlen(needle);
+  int lps[needle_length];
+
+  for(int i = 0; i<needle_length; i++) {
+
+  }
+
+}
